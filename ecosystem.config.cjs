3429a028bfae84965f74077c5197a030be3b2bac
PlanckResearch <admin@planckresearch.com>
2025-08-04 00:20:04 +0200

    Changes in deploy

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,31 +1,14 @@
-require('dotenv').config({ path: __dirname + '/.env' });
-
 module.exports = {
   apps: [{
     name: 'thequanticsapp',
-    script: '/var/www/thequantics/dist/index.html',
-    instances: '2',
-    exec_mode: 'cluster',
+    script: 'npx',
+    args: 'serve -s dist -l 5001',
+    instances: 1,
+    exec_mode: 'fork',
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
-    log_date_format: 'YYYY-MM-DD HH:mm Z',
-    error_file: '/var/log/thequanticsapp.error.log',
-    out_file: '/var/log/thequanticsapp.out.log',
-    merge_logs: true,
-    
-    // Environment variables (development)
     env: {
-      NODE_ENV: 'development',
-      PORT: 5001,
-      STATIC_DIR: '/var/www/thequantics/dist'
-    },
-    
-    // Production environment variable
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 5001,
-      STATIC_DIR: '/var/www/thequantics/dist'
+      NODE_ENV: 'production'
     }
   }]
 };
